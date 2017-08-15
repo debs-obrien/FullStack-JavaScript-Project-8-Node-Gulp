dfdc98daf9c141722d6ecf65f4aef374af03df47
Debbie O'Brien <debs_obrien@yahoo.co.uk>
2017-08-15 19:05:39 +0200

    added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-project-8-gulp
+# Using Gulp to Build a Front End Website
+## FullStack JavaScript Project 8
+### Node.JS, Gulp, Sass
+
+
+* The gulp scripts command concatenates, minifies, and copies all of the project’s JavaScript files into an all.min.js file and copies it into the dist/scripts folder
+* The gulp styles command compiles the project’s SCSS files into CSS, and concatenates and minifies into an all.min.css and copies it into the dist/styles folder
+* The gulp scripts command generates JavaScript source maps
+* The gulp styles command generates CSS source maps
+* The gulp images command copies the optimized images to the dist/content folder
+* The gulp clean command deletes all of the files and folders in the dist folder
+* The gulp build command properly runs the clean, scripts, styles, and images tasks.
+* The clean task fully completes before the scripts, styles, and images tasks are ran
+* The gulp command properly runs the build task as a dependency
+* The gulp command serves the project using a local webserver.
+* The gulp command also listens for changes to any .scss file. When there is a change to any .scss file, the gulp styles command is run, the files are compiled, concatenated and minified to the dist folder, and the browser reloads, displaying the changes
+
+### Installation
+
+```
+$ npm install
+```
+### Use the Following to run the tasks
+```
+gulp scripts
+gulp styles
+gulp images
+gulp build
+gulp
+```
+
+#### By Debbie O'Brien
+15 August 2017
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
@@ -19,45 +18,55 @@ const paths = {
     dist: 'dist'
 };
 
+/* ---------------------------------------------------------------------------------------
+Get js files, create sourcemap, concatanate to global.js
+minify global.js and rename it all.min.js and put in dist/scripts reload if necessary
+---------------------------------------------------------------------------------------*/
 gulp.task("concatJS", function(){
     return gulp.src(`${paths.src}/js/**/*.js`)
         .pipe(maps.init())
         .pipe(concat('global.js'))
         .pipe(maps.write('./'))
         .pipe(gulp.dest(`${paths.src}/js`))
-        .pipe(browserSync.stream());
 });
 gulp.task('scripts', ['concatJS'], function(){
     return gulp.src(`${paths.src}/js/global.js`)
         .pipe(uglify())
         .pipe(rename('all.min.js'))
         .pipe(gulp.dest(`${paths.dist}/scripts`))
+        .pipe(browserSync.stream());
 });
 
+/* ---------------------------------------------------------------------------------------
+ get global.scss and create sourcemap save scss as css
+ minify global.css and rename it all.min.css and put in dist/styles reload if necessary
+ ---------------------------------------------------------------------------------------*/
 gulp.task('compile', function(){
     return gulp.src(`${paths.src}/sass/global.scss`)
         .pipe(maps.init())
         .pipe(sass())
         .pipe(maps.write('./'))
         .pipe(gulp.dest(`${paths.src}/css`))
-        .pipe(browserSync.stream());
 });
 gulp.task('styles',['compile'],  function(){
     return gulp.src(`${paths.src}/css/global.css`)
         .pipe(cleanCSS())
         .pipe(rename('all.min.css'))
         .pipe(gulp.dest(`${paths.dist}/styles`))
+        .pipe(browserSync.stream());
 
 });
-gulp.task('bs-reload', function () {
-    browserSync.reload();
-});
 
+/* ---------------------------------------------------------------------------------------
+delete everything in dist folder
+ ---------------------------------------------------------------------------------------*/
 gulp.task('clean', function(){
     return del('dist');
 });
 
-
+/* ---------------------------------------------------------------------------------------
+ minifiy images using cache and add to folder called content in dist
+ ---------------------------------------------------------------------------------------*/
 gulp.task('images', function(){
     return gulp.src(`${paths.src}/images/**/*.+(png|jpg)`)
         .pipe(cache(imagemin([
@@ -67,28 +76,57 @@ gulp.task('images', function(){
         .pipe(gulp.dest(`${paths.dist}/content`));
 });
 
+/* ---------------------------------------------------------------------------------------
+ minifiy icons using cache and add to dist folder
+ ---------------------------------------------------------------------------------------*/
 gulp.task('icons', function(){
     return gulp.src(`${paths.src}/icons/**/*`)
-        .pipe(imagemin([
+        .pipe(cache(imagemin([
             imagemin.svgo({ plugins: [{ removeViewBox: true }] })
-        ]))
+        ])))
         .pipe(gulp.dest(`${paths.dist}/icons`));
 
 });
+
+/* ---------------------------------------------------------------------------------------
+ refresh broswer useful if you make changes to html or sass or js
+ ---------------------------------------------------------------------------------------*/
 gulp.task('browserSync', function() {
     return browserSync.init({
         server: {
-            baseDir: paths.src
+            baseDir: paths.dist
         },
     })
 });
+
+/* ---------------------------------------------------------------------------------------
+ changes the html to call the new minified css and js
+ then replaces all references to the images folder with content folder
+ add the html to the dist folder
+ call refresh incase any changes have been made
+ ---------------------------------------------------------------------------------------*/
+gulp.task('html', function(){
+    return gulp.src(`${paths.src}/index.html`)
+        .pipe(useref())
+        .pipe(replace('images/', 'content/'))
+        .pipe(gulp.dest(paths.dist))
+        .pipe(browserSync.stream());
+});
+
+/* ---------------------------------------------------------------------------------------
+ watch for any changes to sass, js or html and if so call styles/scripts/html
+ ---------------------------------------------------------------------------------------*/
 gulp.task('watch', ['browserSync'], function (){
     gulp.watch(`${paths.src}/sass/**/*.scss`, ['styles']);
     gulp.watch(`${paths.src}/js/**/*.js`, ['scripts']);
-    gulp.watch([`${paths.src}/*.css`], ['bs-reload']);
-    gulp.watch([`${paths.src}/*.html`], ['bs-reload']);
+    gulp.watch([`${paths.src}/*.html`],['html']);
 });
 
+/* ---------------------------------------------------------------------------------------
+ build for production first cleaning any old dist
+ then run scripts, styles, images and icons
+ finally run the html to change the routes of images and name of minfified files
+ ---------------------------------------------------------------------------------------*/
 gulp.task('build',['clean'], function(){
     gulp.start(['scripts', 'styles', 'images', 'icons']);
     return gulp.src(`${paths.src}/index.html`)
@@ -96,6 +134,11 @@ gulp.task('build',['clean'], function(){
         .pipe(replace('images/', 'content/'))
         .pipe(gulp.dest(paths.dist));
 });
+
+/* ---------------------------------------------------------------------------------------
+ by typing gulp run build and once build is complete
+ start watching and open browser to see changes
+ ---------------------------------------------------------------------------------------*/
 gulp.task('default',['build'], function(){
     gulp.start(['watch', 'browserSync']);
 
